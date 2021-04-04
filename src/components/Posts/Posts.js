30efe88ee9a5cfae08c7ccf439acf53b24028426
Sergio Arreola <s.arreola84@gmail.com>
2021-04-03 17:24:57 -0700

    Full Stack MERN Project Part 1

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  // eslint-disable-next-line no-unused-vars
+  const classes = useStyles();
+
+  console.log(posts);
+
+  return (
+    <>
+      <h1>POSTS</h1>
+      <Post />
+      <Post />
+    </>
+  );
+};
+
+export default Posts;
